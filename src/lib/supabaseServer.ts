3fabef7e35c525c484aed417d65a73e4aab678f3
nja-rasheed <rasheedstudies21@gmail.com>
2026-01-06 22:23:22 +0530

    test

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,8 +1,8 @@
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export async function supabaseServer() {
-  const cookieStore = await cookies();
+export function supabaseServer() {
+  const cookieStore = cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
